2b8a47d012989418db119a1adf11bcc659a0c7a0
mo916607 <Mikael.Ostberg@telenor.se>
2017-08-01 12:37:52 +0200

    Initial commit with sample-code from flow-soapui-plugin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# vault-soapui-plugin
+Vault SoapUI Plugin
+-------------------------------
+
+Plugin that can check-in and check-out vault keys with a [Vault Server](http://vaultproject.io)
+
+[![Build Status](https://travis-ci.org/O5ten/flow-soapui-plugin.svg?branch=master)](https://travis-ci.org/O5ten/flow-vault-plugin)
+
---
+++ src/main/java/se/osten/flow/teststeps/repeat/RepeatTestStepActionGroup.java
@@ -0,0 +1,9 @@
+package se.osten.flow.teststeps.repeat;
+
+import com.eviware.soapui.impl.wsdl.actions.teststep.WsdlTestStepSoapUIActionGroup;
+
+public class RepeatTestStepActionGroup extends WsdlTestStepSoapUIActionGroup {
+    public RepeatTestStepActionGroup() {
+        super("RepeatTestStepActions", "Repeat Test Step Actions");
+    }
+}
